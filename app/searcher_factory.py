93f68b584b3500bcd31dc82fb39952cad6159c0e
Alisher <38488987+anurmanov@users.noreply.github.com>
2022-04-11 12:11:55 +0300

    fixed typo

--- app/searcher_factory.py
+++ app/searcher_factory.py
@@ -5,7 +5,7 @@ from app.searchers.json.json_searcher import JsonSearcher
 
 def searcher_factory(type: str) -> Searcher:
     """
-    This factory function create an object-inheritant of the Searcher class
+    This factory function creates an object-inheritant of the Searcher class
     according to the required type of the searcher ('json',
     might be 'postgres' in the potential future)
     :param type: type of the searcher
